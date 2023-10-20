98a42b336639935ac1bbeff7a35119db59099ee4
artem1912 <artem.savchuk1219@gmail.com>
2023-10-19 20:38:29 -0700

    first commit

---
+++ lib/constants/constants.dart
@@ -0,0 +1,10 @@
+
+import 'package:intl/intl.dart';
+
+String transakBaseUrl = 'https://global.transak.com';
+String transakUrlForPrice = 'https://api.transak.com/api/v2/currencies/price';
+String apiKey = 'YOUR-TRANSAK-API-KEY-HERE';
+
+
+var formatter2 = NumberFormat.decimalPatternDigits();
+
